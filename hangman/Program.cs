7b37e3406efca0e808d4aac5f7932c607a1b81d8
Ginika Okeke <243939796+ginikaifeanyi88-web@users.noreply.github.com>
2026-08-30 15:54:55 +0200

    minor change

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -192,7 +192,7 @@ namespace hangman
                 Console.WriteLine("  |   |");
                 Console.WriteLine("  o   |");
                 Console.WriteLine(@" /|\  |");
-                Console.WriteLine(@" / \   |");
+                Console.WriteLine(@" / \  |");
                 Console.WriteLine("      |");
                 Console.WriteLine("=========");
             }
